fff424ec6e71e4ce26aee0243cda7e9353b7137d
LFseohyeon <m9037m@gmail.com>
2023-03-20 00:34:44 +0900

    김서현 커밋

--- src/main/resources/static/js/mypage/my-qna.js
+++ src/main/resources/static/js/mypage/my-qna.js
@@ -1,12 +1,13 @@
+/* 슬라이드 */
 $(document).ready(function(){
     $("#flip").click(function(){
       $("#panel").slideToggle("slow");
     });
   });
 
-    /* 모달 */
+    /* 수정모달 */
     const btn = document.querySelector(".modal-btn");
-    const container = document.querySelector("#modal");
+    const container = document.querySelector(".change-modal");
     const close = document.querySelector(".close-btn");
     
     //모달창 열기
@@ -23,5 +24,27 @@ $(document).ready(function(){
     window.onclick = function(event) {
         if(event.target == container) {
             container.style.display = "none";
+        }
+    };
+
+    /* 삭제 모달 */
+    const deleteBtn = document.querySelector(".delete-btn");
+    const deleteContainer = document.querySelector(".deleteModal");
+    const deleteClose = document.querySelector(".pay-popup-check");
+    
+    //모달창 열기
+    deleteBtn.addEventListener("click", function(){
+        deleteContainer.style.display="block";
+    });
+
+    //모달창 닫기
+    deleteClose.addEventListener("click", function(){
+        deleteContainer.style.display="none";
+    });
+
+    //모달창 외부 클릭 시 닫기
+    window.onclick = function(event) {
+        if(event.target == deleteContainer) {
+            deleteContainer.style.display = "none";
         }
     };
