bee6d017a720d5d3f646f01ba028f5f44f08f7cb
dev-hyunjin <disappointed123419@gmail.com>
2023-03-24 01:09:22 +0900

    회원가입 작업 중

--- src/main/java/com/app/jimcarry/controller/UserController.java
+++ src/main/java/com/app/jimcarry/controller/UserController.java
@@ -1,15 +1,42 @@
 package com.app.jimcarry.controller;
 
+import com.app.jimcarry.domain.vo.UserVO;
+import com.app.jimcarry.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user/*")
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
+    private final UserService userService;
 
     @GetMapping("join")
     public String join() {
         return "/joinLogin/joinForm";
     }
+
+    @PostMapping("checkIdentificationDuplicate")
+    @ResponseBody
+    public boolean checkIdentificationDuplicate(String userIdentification) {
+        return userService.checkIdentificationDuplicate(userIdentification);
+    }
+
+    @PostMapping("checkEmailDuplicate")
+    @ResponseBody
+    public boolean checkEmailDuplicate(String userEmail) {
+        return userService.checkEmailDuplicate(userEmail);
+    }
+
+    @PostMapping("join")
+    public void join(UserVO userVO) {
+        log.info(userVO.getUserBirth());
+        userVO.setUserBirth(userVO.getUserBirth().replace(",", "-"));
+        log.info(userVO.getUserBirth());
+        log.info(userVO.getUserGender());
+    }
+
 }
