5e5bb14b5d6c58e9464af5e0138e4a3577834f6c
dev-hyunjin <disappointed123419@gmail.com>
2023-03-25 19:03:43 +0900

    로그인 완료

--- src/main/resources/static/js/joinLogin/login.js
+++ src/main/resources/static/js/joinLogin/login.js
@@ -4,12 +4,36 @@ const $checkButton = $('.popup-check');
 
 const $loginButton = $('.loginButton');
 
+if(window.location.search) {
+    $modalText.text('아이디, 비밀번호를 확인해주세요.');
+    $modal.css("visibility", "visible");
+}
+
 $loginButton.on('click', ()=>{
-    if(!$($('.idPwInput')[0]).val() || !$($('.idPwInput')[1]).val()) {
+    const $userIdentification = $('.idPwInput').eq(0).val();
+    const $userPassword = $('.idPwInput').eq(1).val();
+
+    if(!$userIdentification || !$userPassword) {
         $modal.css("visibility", "visible");
         return;
     }
-    console.log('섭밋')
+
+    // $.ajax({
+    //     url: "/user/login",
+    //     type: "post",
+    //     data: { userIdentification : $userIdentification, userPassword : $userPassword },
+    //     success: function(result) {
+    //         if(result) {
+    //             $modalText.text("사용 가능 합니다.");
+    //             $($('.duplicateBox')[1]).attr('disabled', true);
+    //             $('.emailInput').attr('readonly', true);
+    //         } else {
+    //             $modalText.text("사용 불가능 합니다.");
+    //             $('.emailInput').val('');
+    //         }
+    //     }
+    // });
+    loginForm.submit();
 });
 $checkButton.on("click", ()=>{
     $modal.css("visibility", "hidden");
