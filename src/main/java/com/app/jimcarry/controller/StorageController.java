f8d4cb5077150e82bddc147433b27c04e8c32931
coder-juyeon <test.2678ju@gmail.com>
2023-03-27 04:02:56 +0900

    상세페이지 창고목록 반절완료, 후기목록 수정중

--- src/main/java/com/app/jimcarry/controller/StorageController.java
+++ src/main/java/com/app/jimcarry/controller/StorageController.java
@@ -32,48 +32,18 @@ public class StorageController {
     @GetMapping("register")
     public String register() { return "/storageRegister/storageRegister"; }
 
-    /*@PostMapping("detail")
-    public RedirectView detail(StorageVO storageVO, RedirectAttributes redirectAttributes){
-        storageService.getStorage(1L);
-        redirectAttributes.addAttribute("storageId", storageVO.getStorageId());
-        return new RedirectView("/detail-info/detail-info");
-    }*/
-
-    /*@GetMapping("detail")
-    public String detail() {
-        return "/detail-info/detail-info";
-    }*/
-
-    @GetMapping("detail")
-    public String myBox(Criteria criteria, Model model) {
-
-        /* 한 페이지에 보여줄 게시글 개수 */
-        int amount = 3;
-        /* 검색된 결과의 총 개수 */
-        int total = 0;
-
-        /* 추후에 setUserId 세션으로 변경 */
-        SearchDTO searchDTO = new SearchDTO();
-        /*searchDTO.setTypes(new ArrayList<>(Arrays.asList("userId")));
-        searchDTO.setUserId(2L);*/
-
-        /*log.info(criteria.getPage() + "............");
-        log.info(criteria.toString());*/
-
-        PageDTO pageDTO = null;
-
-//         페이지 번호가 없을 때, 디폴트 1페이지
-        if (criteria.getPage() == 0) {
-            criteria.create(1, amount);
-        } else criteria.create(criteria.getPage(), amount);
-
-        total = reviewService.getTotal();
-        pageDTO = new PageDTO().createPageDTO(criteria, total, searchDTO);
-        model.addAttribute("total", total);
-        model.addAttribute("reviews", reviewService.getList(pageDTO));
-        model.addAttribute("pagination", pageDTO);
-
-        return "/detail-info/detail-info";
+    /* 전체 창고 목록 검색 */
+    @GetMapping("search")
+    public String searchAll() {
+        return "/main/search-page";
     }
+
+    /* 헤더의 지역별을 눌렀을 때 지역별 창고 목록 검색 */
+    /*@GetMapping("search")
+    public RedirectView searchBy(StorageVO storageVO,RedirectAttributes redirectAttributes) {
+        *//**//*
+        redirectAttributes.addAttribute("storageAddress", storageVO.getStorageAddress());
+        return new RedirectView("/main/search-page");
+    }*/
 
 }
