5a47ad8654bcee51b7c5e39b8610175dbd08df0b
LFseohyeon <m9037m@gmail.com>
2023-03-28 02:04:15 +0900

    김서현 결제 커밋

--- src/main/java/com/app/jimcarry/mapper/PaymentMapper.java
+++ src/main/java/com/app/jimcarry/mapper/PaymentMapper.java
@@ -1,21 +1,27 @@
 package com.app.jimcarry.mapper;
 
+import com.app.jimcarry.domain.dto.PageDTO;
 import com.app.jimcarry.domain.dto.PaymentDTO;
 import com.app.jimcarry.domain.vo.PaymentVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface PaymentMapper {
+    //    추가
+    public void insert(PaymentVO paymentVO);
 
-//    //    결제조회
-//    public PaymentDTO select(Long payId);
-//
-//    //    회원조회
-//    public PaymentDTO select(Long userId);
-//
-//    //    창고조회
-//    public PaymentDTO select(Long storageId);
+    //    조회
+    public PaymentVO select(Long payId);
 
-    //    결제 날짜 조회
-    public PaymentDTO select(String paymentDate);
+    //    전체조회
+    public List<PaymentDTO> selectAll(@Param("page") PageDTO paymentDTO);
+
+    //    수정
+    public void update(PaymentVO paymentVO);
+
+    //    삭제
+    public void delete(Long payId);
 }
--- src/main/java/com/app/jimcarry/service/PaymentService.java
+++ src/main/java/com/app/jimcarry/service/PaymentService.java
@@ -1,7 +1,57 @@
 package com.app.jimcarry.service;
 
+import com.app.jimcarry.aspect.annotation.Encryption;
+import com.app.jimcarry.aspect.annotation.LogStatus;
+import com.app.jimcarry.domain.dao.PaymentDAO;
+import com.app.jimcarry.domain.dto.PageDTO;
+import com.app.jimcarry.domain.dto.PaymentDTO;
+import com.app.jimcarry.domain.vo.PaymentVO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class PaymentService {
+    private final PaymentDAO paymentDAO;
+
+    //    추가
+    @LogStatus
+    @Transactional(rollbackFor = Exception.class)
+    public void register(PaymentVO paymentVO) {paymentDAO.save(paymentVO);
+    }
+
+
+    //    조회
+    @LogStatus
+    public PaymentVO getPayment(Long payId) {
+        return paymentDAO.findById(payId);
+    }
+
+
+    //    전체조회
+    @LogStatus
+    public List<PaymentDTO> getList(PageDTO pageDTO) {
+        return paymentDAO.findAll(pageDTO);
+    }
+
+
+    //    수정
+    @Encryption
+    @LogStatus
+    @Transactional
+    public void updatePayment(PaymentVO paymentVO) {paymentDAO.setPaymentVO(paymentVO);}
+
+
+    //    삭제
+    @LogStatus
+    public void removePayment(Long payId) {
+        Optional.ofNullable(paymentDAO.findById(payId)).orElseThrow();
+        paymentDAO.deleteById(payId);
+    }
 }
