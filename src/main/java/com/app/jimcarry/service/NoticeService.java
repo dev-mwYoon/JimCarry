a307e4c4cbfdfaa19f876c0e7229beace2a44f87
coding-moon <dawn43214@gmail.com>
2023-03-29 21:05:21 +0900

    공지사항 목록 상세보기 끝

--- src/main/java/com/app/jimcarry/service/NoticeService.java
+++ src/main/java/com/app/jimcarry/service/NoticeService.java
@@ -1,5 +1,6 @@
 package com.app.jimcarry.service;
 
+import com.app.jimcarry.aspect.annotation.LogStatus;
 import com.app.jimcarry.domain.dao.NoticeDAO;
 import com.app.jimcarry.domain.dto.PageDTO;
 import com.app.jimcarry.domain.vo.NoticeVO;
@@ -21,13 +22,16 @@ public class NoticeService {
     private final UserVO userVO;
 
     /* 공지사항 전체 목록 조회*/
+    @LogStatus
     public List<NoticeVO> getList(PageDTO pageDTO){
         return noticeDAO.findAll(pageDTO);
     }
 
     /*공지사항 전체 개수*/
+    @LogStatus
     public int getTotal(){return noticeDAO.findTotal();}
 
+    @LogStatus
     public String sendRandomNumber(String userPhone) throws CoolsmsException {
         String api_key = "NCSW9JM1RREOSKPR";
         String api_secret = "CWPWKNOLA3D0FD94JOY4W6Q2SBYXVSOK";
@@ -35,10 +39,10 @@ public class NoticeService {
 
 //        Random random = new Random();
 //        String numStr = "";
-        String context = "안녕하세요 답변수신이 완료되었습니다";
+        String context = "안녕하세요 짐캐리입니다 답변이 완료되었습니다";
 
 //        회원가입에서 -가 붙어서 오기 때문에 떼어주는것
-     /*   String userPhone = userPhone.replace("-", "");*/
+       userPhone = userVO.getUserPhone().replace("-", "");
 
        /* for(int i = 0; i < 6; i++) {
             String number = Integer.toString(random.nextInt(10));
@@ -49,12 +53,19 @@ public class NoticeService {
         params.put("to", userPhone);    // 수신전화번호 (ajax로 view 화면에서 받아온 값으로 넘김)
         params.put("from", "01029670403");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
         params.put("type", "sms");
-        params.put("text", context +  "입니다.");
+        params.put("text", context);
 
         coolsms.send(params); // 메시지 전송
 
         return context;
     }
+
+        /*조회*/
+        @LogStatus
+        public NoticeVO getNotice(Long noticeId) {return noticeDAO.findById(noticeId);}
+
+
+
 
 }
 
--- src/main/resources/static/js/notice/notice.js
+++ src/main/resources/static/js/notice/notice.js
@@ -6,7 +6,7 @@ const createDOM = function(notices){
     console.log("들어왓어?");
     let text = `
              <li>
-                  <a href="#">
+                  <a href="/notice/detail/${notices.noticeId}">
                         <div class="notice-list-table-content-div">
                             <div class="notice-list-table-content-number">${notices.noticeId}</div>
                             <div class="notice-list-table-content-title">${notices.noticeTitle}</div>
