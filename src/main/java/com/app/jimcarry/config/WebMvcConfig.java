7c82fdf3098ce200e766c2e805eb52f14609e936
kmg2331 <kmg2331@gmail.com>
2023-04-03 00:32:49 +0900

    버그 수정 및 창고 등록 페이지, 결제 페이지도 쿠키 적용되도록 확장

--- src/main/java/com/app/jimcarry/config/WebMvcConfig.java
+++ src/main/java/com/app/jimcarry/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * 컨트롤러에 요청이 닿기 전에 처리하 인터셉터를 WebMvcConfigurer를 통해
  * 원하는 url에 등록하는 설정 클래스이다.
- * */
+ */
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -18,8 +18,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/users/mypage/**"); // 적용할 URL 패턴
+        registry.addInterceptor(authInterceptor) // 적용할 URL 패턴
+                .addPathPatterns("/users/mypage/**",
+                        "/main",
+                        "/storages/register"/*, "/pay/payment"*/);
 //                .excludePathPatterns("/login/**"); // 제외할 URL 패턴
     }
 }
