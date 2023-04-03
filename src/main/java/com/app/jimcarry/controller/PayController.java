28aaccf9a4e231a8ba0966442e8ddebe4186061f
LFseohyeon <m9037m@gmail.com>
2023-04-03 17:11:11 +0900

    서현 결제 수정

--- src/main/java/com/app/jimcarry/controller/PayController.java
+++ src/main/java/com/app/jimcarry/controller/PayController.java
@@ -1,5 +1,6 @@
 package com.app.jimcarry.controller;
 
+import com.app.jimcarry.domain.vo.PaymentVO;
 import com.app.jimcarry.domain.vo.UserVO;
 import com.app.jimcarry.service.PaymentService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -30,10 +32,33 @@ public class PayController {
     }
     @GetMapping("pay-reserve")
     public String payReserve(String paymentAmount, Model model, HttpSession httpSession){
-        log.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" + paymentAmount);
         model.addAttribute("userVO", (UserVO)httpSession.getAttribute("user"));
         model.addAttribute("paymentAmount", paymentAmount);
         return "/pay/pay-reserve";
     }
+
+//    @PostMapping("/payment")
+//    public String pay(PaymentVO paymentVO, Model model, HttpSession httpSession) {
+//        model.addAttribute("user", paymentService.getUser(((UserVO)(httpSession.getAttribute("user"))).getUserId()));
+//
+//        paymentService.register(paymentVO);
+//
+//        model.addAttribute("storageTitle", paymentVO.getStorageTitle());
+//        model.addAttribute("paymentMonth", paymentVO.getPaymentMonth());
+//        model.addAttribute("paymentAmount", paymentVO.getPaymentAmount());
+//
+//        return "/pay/payment";
+//    }
+
+//    @PostMapping("payRegister")
+//    @ResponseBody
+//    public String payRegister(String paymentAmount) {
+//        PaymentVO paymentVO = new PaymentVO();
+//        paymentVO.setPaymentAmount();
+//        paymentVO.setStorageId();
+//        paymentVO.setUserId();
+//        paymentVO.setPayId();
+//        paymentService.register();
+//    }
 
 }
