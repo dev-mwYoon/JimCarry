fed22f2bd3b3156075d1d5da4e70f8ce5e0b1e1f
LFseohyeon <m9037m@gmail.com>
2023-04-04 11:09:09 +0900

    서현 커밋

--- src/main/java/com/app/jimcarry/controller/PayController.java
+++ src/main/java/com/app/jimcarry/controller/PayController.java
@@ -1,18 +1,17 @@
 package com.app.jimcarry.controller;
 
 import com.app.jimcarry.domain.vo.PaymentVO;
+import com.app.jimcarry.domain.vo.StorageVO;
 import com.app.jimcarry.domain.vo.UserVO;
 import com.app.jimcarry.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/pay/*")
@@ -23,15 +22,16 @@ public class PayController {
 
 //    /*결제페이지*/
     @PostMapping("/payment")
-    public String pay(String storageTitle, String paymentMonth, String paymentAmount, Model model, HttpSession httpSession) {
+    public String pay(String storageTitle, String paymentMonth, String paymentAmount, String storageId, Model model, HttpSession httpSession) {
         model.addAttribute("user", paymentService.getUser(((UserVO)(httpSession.getAttribute("user"))).getUserId()));
         model.addAttribute("storageTitle",storageTitle);
+        model.addAttribute("storageId",storageId);
         model.addAttribute("paymentMonth", paymentMonth);
         model.addAttribute("paymentAmount", paymentAmount);
-        log.info("asdfasdfsadfadfs sa fasdf asd as sf as das paymentAmount: " + paymentAmount);
-
         return "/pay/payment";
     }
+
+//    결제완료페이지
     @GetMapping("pay-reserve")
     public String payReserve(String paymentAmount, Model model, HttpSession httpSession){
         model.addAttribute("userVO", (UserVO)httpSession.getAttribute("user"));
@@ -39,28 +39,15 @@ public class PayController {
         return "/pay/pay-reserve";
     }
 
-//    @PostMapping("/payment")
-//    public String pay(PaymentVO paymentVO, Model model, HttpSession httpSession) {
-//        model.addAttribute("user", paymentService.getUser(((UserVO)(httpSession.getAttribute("user"))).getUserId()));
-//
-//        paymentService.register(paymentVO);
-//
-//        model.addAttribute("storageTitle", paymentVO.getStorageTitle());
-//        model.addAttribute("paymentMonth", paymentVO.getPaymentMonth());
-//        model.addAttribute("paymentAmount", paymentVO.getPaymentAmount());
-//
-//        return "/pay/payment";
-//    }
-
-//    @PostMapping("payRegister")
-//    @ResponseBody
-//    public String payRegister(String paymentAmount) {
-//        PaymentVO paymentVO = new PaymentVO();
-//        paymentVO.setPaymentAmount();
-//        paymentVO.setStorageId();
-//        paymentVO.setUserId();
-//        paymentVO.setPayId();
-//        paymentService.register();
-//    }
+    @PostMapping("/payRegister")
+    @ResponseBody
+    public String payRegister(String paymentAmount, String storageAmount, HttpSession httpSession) {
+        PaymentVO paymentVO = new PaymentVO();
+        paymentVO.setPaymentAmount(Integer.parseInt(paymentAmount));
+        paymentVO.setUserId(((UserVO)httpSession.getAttribute("user")).getUserId());
+        paymentVO.setStorageId(Long.valueOf(storageAmount));
+        paymentService.register(paymentVO);
+        return "/pay/pay-reserve?paymentAmount=" + paymentAmount;
+    }
 
 }
