9edcfce523e703e246f4e168e62c2dea9c251a9c
LFseohyeon <m9037m@gmail.com>
2023-04-04 17:43:28 +0900

    서현 결제 최종커밋이길...

--- src/main/java/com/app/jimcarry/controller/PayController.java
+++ src/main/java/com/app/jimcarry/controller/PayController.java
@@ -1,7 +1,6 @@
 package com.app.jimcarry.controller;
 
 import com.app.jimcarry.domain.vo.PaymentVO;
-import com.app.jimcarry.domain.vo.StorageVO;
 import com.app.jimcarry.domain.vo.UserVO;
 import com.app.jimcarry.service.PaymentService;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
-import java.util.UUID;
 
 @Controller
 @RequestMapping("/pay/*")
@@ -30,7 +28,8 @@ public class PayController {
         model.addAttribute("paymentAmount", paymentAmount);
         return "/pay/payment";
     }
-    @GetMapping("pay-reserve")
+
+    @GetMapping("/pay-reserve")
     public String payReserve(String paymentAmount, Model model, HttpSession httpSession){
         model.addAttribute("userVO", (UserVO)httpSession.getAttribute("user"));
         model.addAttribute("paymentAmount", paymentAmount);
@@ -39,11 +38,12 @@ public class PayController {
 
     @PostMapping("/payRegister")
     @ResponseBody
-    public String payRegister(String paymentAmount, String storageAmount, HttpSession httpSession) {
+    public String payRegister(String paymentAmount, String storageId, HttpSession httpSession) {
         PaymentVO paymentVO = new PaymentVO();
+        log.info(storageId+":::::::::::::::::::::::::::::::::::::::::::::::::::");
         paymentVO.setPaymentAmount(Integer.parseInt(paymentAmount));
         paymentVO.setUserId(((UserVO)httpSession.getAttribute("user")).getUserId());
-        paymentVO.setStorageId(Long.valueOf(storageAmount));
+        paymentVO.setStorageId(Long.valueOf(storageId));
         paymentService.register(paymentVO);
         return "/pay/pay-reserve?paymentAmount=" + paymentAmount;
     }
