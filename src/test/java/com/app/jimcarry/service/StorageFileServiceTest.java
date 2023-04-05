ecd71237b2942ce56fbfce0312adf1d08784ce05
coder-juyeon <test.2678ju@gmail.com>
2023-04-05 12:52:50 +0900

    상세페이지 이미지 클릭하면 바뀜

--- src/test/java/com/app/jimcarry/service/StorageFileServiceTest.java
+++ src/test/java/com/app/jimcarry/service/StorageFileServiceTest.java
@@ -1,11 +1,14 @@
 package com.app.jimcarry.service;
 
 import com.app.jimcarry.domain.vo.FileVO;
+import com.app.jimcarry.domain.vo.ReviewFileVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootTest
 @Slf4j
