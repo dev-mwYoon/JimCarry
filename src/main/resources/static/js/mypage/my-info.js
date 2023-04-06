780b085b42d12dafedeb76b52007594af7e09175
dev-hyunjin <disappointed123419@gmail.com>
2023-04-06 20:58:14 +0900

    마이페이지 생일 / 로 들어가는거 수정

--- src/main/resources/static/js/mypage/my-info.js
+++ src/main/resources/static/js/mypage/my-info.js
@@ -231,7 +231,7 @@ $("div.sizeSelect").on("click", function () {
 
 $(".amend-birth-input-text").eq(2).on("blur", function () {
     let $date = $(".amend-birth-input-text");
-    let format = `${$date.eq(0).val()}/${$date.eq(1).val()}/${$date.eq(2).val()}`;
+    let format = `${$date.eq(0).val()}-${$date.eq(1).val()}-${$date.eq(2).val()}`;
     $("#userBirth").val(format);
 })
 
